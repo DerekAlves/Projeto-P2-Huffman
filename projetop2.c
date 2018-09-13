d392a69c2bb1df8ed3ae62e2fba646728dffea18
aldemirfilho <43156661+aldemirfilho@users.noreply.github.com>
2018-09-13 11:34:27 -0300

    bits ordenados com fila de prioridade sem hash

--- projetop2.c
+++ projetop2.c
@@ -3,23 +3,10 @@
 #include <string.h>
 #define num_prime 257
 
-typedef struct element
-{
-	void *item;
-	void *freq;
-	struct Node *next;
-	struct Node *previous;
-}Node;
-
 typedef struct node_next
 {
 	struct Node_next *next;
 }Node_next;
-
-typedef struct hash_table
-{
-	Node *table[10000];
-}Hash_Table;
 
 typedef struct node_down
 {
@@ -41,34 +28,6 @@ typedef struct Queue
   Element_queue *back;
   int size;
 }Queue;
-
-Node *newNode(void *x)
-{
-    Node *node = (Node*) malloc(sizeof(Node));
-    node->item = x;
-    node->freq = 0;
-    node->next = NULL;
-    node->previous = NULL;
-    return node;
-}
-
-Node_next *new_index()
-{
-	Node_next *node = (Node_next*) malloc(sizeof(Node_next));
-	node->next = NULL;
-	return node;
-}
-
-Hash_Table *create_hash_table()
-{
-	int i;
-	Hash_Table *new_ht = (Hash_Table*) malloc(sizeof(Hash_Table));
-	for(i = 0; i < 10000; i++)
-	{
-		new_ht->table[i] = new_index();
-	}
-	return new_ht;
-}
 
 Queue *createQueue(){
   Queue *queue = (Queue*)malloc(sizeof(Queue));
@@ -125,61 +84,13 @@ Element_queue *dequeue(Queue *queue){
   }
 }
 
-Node *add_element(Node *inicio, void *item, void *freq)
-{
-	Node *node = newNode(item);
-	if(inicio->next == NULL)														
-	{
-		node->item = item;
-		node->freq = freq;
-		node->next = NULL;
-		node->previous = inicio;
-		inicio->next = node;
-		return inicio;
-	}
-}
-
-void put(Hash_Table *ht, void *item, void *freq)
-{
-	int index;
-	int item_aux = item;
-	index = item_aux % num_prime;
-	ht->table[index] = add_element(ht->table[index], item, freq);
-}
-
-void print_hash(Hash_Table *ht)
-{
-	int i;
-	Node *aux;
-	for(i = 0; i < num_prime; i++)
-	{
-		aux = ht->table[i];
-		if(aux->next == NULL)
-		{
-			printf("%d -> VAZIO\n", i);
-		}
-		else
-		{
-			aux = aux->next;
-			printf("%d -> ", i);
-	    	while(aux != NULL)
-	    	{
-		        printf("%d -> ", aux->freq);
-		        aux = aux->next;
-	  	  	}
-	  	  	printf("VAZIO\n");
-	  	}
-	}
-    printf("\n");	
-}
-
 void print_queue(Queue *queue)
 {
 	if(queue == NULL || queue->size == 0) return;
 	Element_queue *element = queue->front;
 	while(element != NULL)
 	{
-		printf("%d ", element->item);
+		printf("%x ", element->item);
 		printf("%d\n", element->freq);
 		element = element->next;
 	}
@@ -192,7 +103,6 @@ int main ()
 	unsigned char s;
 	FILE *f = fopen("japao.png", "r");
 	FILE *p = fopen ("out.txt","a");
-	Hash_Table *ht = create_hash_table();
 	Queue *queue = createQueue();
 	while (fscanf(f, "%c",&s) != EOF)
 	{
@@ -200,9 +110,6 @@ int main ()
 	}
 	for(int i = 0; i < 256; i++) 
 	{
-		//printf("%d %c %d", i, i, freq[i]);
-		//printf("\n");
-		//put(ht, i, freq[i]);
 		enqueue(queue, i, freq[i]);
 	}
 	//print_hash(ht);
